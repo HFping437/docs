5c124237919cc1467ecddfc415c0b85c024c6e22
Hanfuping <1285859637@qq.com>
2022-09-27 11:25:54 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -32,7 +32,7 @@ module.exports = {
       home: {
         title: "Han's Blog",
         subtitle: '好好生活，慢慢相遇',
-        headerImage: '/bg.png'
+        headerImage: '/bg1.png'
       },
       tags: {
         title: 'Tags',
